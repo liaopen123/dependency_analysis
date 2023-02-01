633760a7e7913bf0c5cdc318b25080677680c334
Pony <467969@qq.com>
2023-02-01 22:46:18 +0800

    fix:主页改成返回数据

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send({code: 200, message: "success"});
 });
 
 module.exports = router;
